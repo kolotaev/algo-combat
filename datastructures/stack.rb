639ae0a34b3d7c3536662057d0b03273184ba8c4
kolotaev <kolotaev@tut.by>
2016-12-21 23:23:41 +0300

    stack - linked-list implementation

--- datastructures/stack.rb
+++ datastructures/stack.rb
@@ -1,8 +1,19 @@
+# Stack is a general data-structure that implies FIFO behaviour.
+# Has interface:
+# - push
+# - pop
+# - empty?
+# - size
+#
+# Can have several implementations:
+# - Array
+# - Linked List
+#
+
+# Array implementation of a stack.
 class StackArray
   def initialize
     @storage = []
-    @next = nil
-    @first = nil
   end
 
   def push(element)
@@ -10,7 +21,7 @@ class StackArray
   end
 
   def pop
-    @storage.last
+    @storage.delete_at(@storage.size - 1)
   end
 
   def size
@@ -19,5 +30,33 @@ class StackArray
 
   def empty?
     @storage.empty?
+  end
+end
+
+# Linked-List implementation of a stack.
+class StackLinkedList
+  attr_reader :size
+
+  def initialize
+    @first = {}
+    @size = 0
+  end
+
+  def push(element)
+    @first = { data: element, previous: @first.empty? ? nil : @first }
+    @size += 1
+  end
+
+  def pop
+    data = @first[:data]
+    unless @first.empty?
+      @first = @first[:previous]
+      @size -= 1
+    end
+    data
+  end
+
+  def empty?
+    @first.empty?
   end
 end
