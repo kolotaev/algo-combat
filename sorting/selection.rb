460a3de4ae58a803284fda05b9f28063a9f61c20
kolotaev <kolotaev@tut.by>
2016-12-26 18:37:40 +0300

    sorting tests

---
+++ sorting/selection.rb
@@ -0,0 +1,20 @@
+#
+# Selection sort is the most trivial sort.
+# Time complexity is O(n^2).
+# Actually (n^2)/2.
+#
+# Applications:
+# - only for small sets as a brute force approach.
+#
+class Selection
+  def sort(data)
+    n = 0
+    (0..data.size).each do |i|
+      min = data[n]
+      (i..data.size).each do |j|
+
+      end
+    end
+    data
+  end
+end
---
+++ tests/tc_sorting.rb
@@ -0,0 +1,60 @@
+require_relative '../sorting/selection'
+require 'test/unit'
+
+class TestSorting < Test::Unit::TestCase
+
+  def setup
+    @classes = [
+      Selection
+    ]
+  end
+
+  def test_empty
+    @classes.each do |klass|
+      subject = klass.send(:new)
+      data = []
+      assert_equal([], subject.sort(data))
+    end
+  end
+
+  def test_one_element
+    @classes.each do |klass|
+      subject = klass.send(:new)
+      data = [0]
+      assert_equal([0], subject.sort(data))
+    end
+  end
+
+  def test_equal_elements
+    @classes.each do |klass|
+      subject = klass.send(:new)
+      data = [44, 44, 44]
+      assert_equal([44, 44, 44], subject.sort(data))
+    end
+  end
+
+  def test_two_elements
+    @classes.each do |klass|
+      subject = klass.send(:new)
+      data = [7, 1]
+      assert_equal([1, 7], subject.sort(data))
+    end
+  end
+
+  def test_two_elements_already_sorted
+    @classes.each do |klass|
+      subject = klass.send(:new)
+      data = [1, 7]
+      assert_equal([1, 7], subject.sort(data))
+    end
+  end
+
+  def test_many_elements
+    @classes.each do |klass|
+      subject = klass.send(:new)
+      data = [1, 0, 7, 8, 66, -7, 5, 54, 0, 3, 1000, 7]
+      assert_equal([-7, 0, 0, 1, 3, 5, 7, 8, 54, 66, 1000], subject.sort(data))
+    end
+  end
+
+end
