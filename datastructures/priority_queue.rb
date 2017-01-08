fa2c298a0192a0eb6d36869a31b00923e686a2c4
kolotaev <kolotaev@tut.by>
2017-01-08 17:30:32 +0300

    add priority-queue

---
+++ datastructures/priority_queue.rb
@@ -0,0 +1,17 @@
+# Priority Queue is a data type that implies FIFO behaviour.
+#  Allows to immediately extract min pr max element.
+# Has interface:
+# - insert
+# - get_max
+# - empty?
+# - size
+#
+# Usually is implemented with binary heap (that uses array underneath)
+#
+# Usage:
+# - scheduling of processes
+# - event-driven simulation
+# - monitoring
+
+class PriorityQueue
+end
